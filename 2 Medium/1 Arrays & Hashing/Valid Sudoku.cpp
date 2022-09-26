a06c61dc21ee14af364bf8c5cded3d60bf8e15f5
Christopher Suga <chrissuga626@gmail.com>
2022-09-26 16:32:07 -0700

    Update Valid Sudoku.cpp

--- 2 Medium/1 Arrays & Hashing/Valid Sudoku.cpp
+++ 2 Medium/1 Arrays & Hashing/Valid Sudoku.cpp
@@ -9,43 +9,30 @@
 
 class Solution
 {
-public:
-    bool isValidSudoku(vector<vector < char>>& board)
-    {
-        const int cnt = 9;
-        //bool matrices for the rows columns and subsquares
-        //rows represent row col and subsquares
-        //columns represent the numbers 1-9
-        bool row[cnt][cnt] = { false
-        };
-        bool col[cnt][cnt] = { false
-        };
-        bool sub[cnt][cnt] = { false
-        };
-        //nested for loop to iterate over the board
-        for (int r = 0; r < cnt; ++r)
+    public:
+        bool isValidSudoku(vector<vector < char>> &board)
         {
-            for (int c = 0; c < cnt; ++c)
+            int N = 9;
+            unordered_set<char> row[N];
+            unordered_set<char> col[N];
+            unordered_set<char> sub[N];
+
+            for (int r = 0; r < N; r++)
             {
-                //if the value is not a period then it means it is a number and enters the block
-                if (board[r][c] != '.')
+                for (int c = 0; c < N; c++)
                 {
-                    //char to num idx the -1 is there because the matrix includes 0 but sudoku is 1-9
-                    int idx = board[r][c] - '0' - 1;
-                    //area calculates what subsquare we are in
-                    int area = (r / 3) * 3 + (c / 3);
-                    //if the value already exists in the row column or subsquare then returns false
-                    if (row[r][idx] || col[c][idx] || sub[area][idx])
+                    char val = board[r][c];
+                    if (val != '.')
                     {
-                        return false;
+                        int subPos = (r / 3) *3 + c / 3;
+                        if (row[r].count(val) || col[c].count(val) || sub[subPos].count(val))
+                            return false;
+                        row[r].insert(val);
+                        col[c].insert(val);
+                        sub[subPos].insert(val);
                     }
-                    //if the value doesnt exist then it adds it to their respective location
-                    row[r][idx] = true;
-                    col[c][idx] = true;
-                    sub[area][idx] = true;
                 }
             }
+            return true;
         }
-        return true;
-    }
 };
